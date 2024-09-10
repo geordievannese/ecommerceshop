be81b4637e8bc871a8890544ce8f241127eee0af
Geordie Vannese <geordievannese01@gmail.com>
2024-09-10 18:24:44 +0700

    PWS deployment

--- ecommerceshop/settings.py
+++ ecommerceshop/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = "django-insecure-*%4-(1xq!n%n^%w64)7cxe%dm9sn9hqnw=l*++fqic%pjlkk+v
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ["localhost", "127.0.0.1"]
+ALLOWED_HOSTS = ["localhost", "127.0.0.1","geordie-vannese-ecommerceshop.pbp.cs.ui.ac.id"]
 
 
 # Application definition
